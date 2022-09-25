a95493ea17469c812f41c7a856a9e95234ef2267
weilele0 <100892281+weilele0@users.noreply.github.com>
2022-09-25 18:39:55 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -65,9 +65,9 @@ const USER_CONFIG = {
       // 想要发送的人的名字
       name: '老婆0',
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: '',
+      id: 'o9I3a56jIQwTaNyjrHLWsf6yifII',
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: '',
+      useTemplateId: 'amyDtlPAbdE5pxyfsreQFDaZaH3yxxyq7y6ybUHN0S4',
       // 所在省份
       province: '广东',
       // 所在城市
